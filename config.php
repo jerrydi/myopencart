f1d32db52507370b6bee5077f52f9291c0c12ad5
dijianlei <di_jianlei@163.com>
2015-11-25 09:24:27 +0800

    created

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/opencart/');
+
+// DIR
+define('DIR_RUNTIMEPATH', 'C:\app\wamp\www\opencart/runtime/catalog/');
+define('DIR_APPLICATION', 'C:\app\wamp\www\opencart/catalog/');
+define('DIR_SYSTEM', 'C:\app\wamp\www\opencart/system/');
+define('DIR_DATABASE', 'C:\app\wamp\www\opencart/system/database/');
+define('DIR_LANGUAGE', 'C:\app\wamp\www\opencart/catalog/language/');
+define('DIR_TEMPLATE', 'C:\app\wamp\www\opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\app\wamp\www\opencart/system/config/');
+define('DIR_IMAGE', 'C:\app\wamp\www\opencart/image/');
+define('DIR_CACHE', 'C:\app\wamp\www\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\app\wamp\www\opencart/download/');
+define('DIR_LOGS', 'C:\app\wamp\www\opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123-abc');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
